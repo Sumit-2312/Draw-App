0963a857718f08b1d47addcd3779d5bdab3a7536
Sumit <sumitsongh160@gmail.com>
2025-02-27 22:24:59 +0530

    Added the signin endpoint with db and zod

--- apps/http-server/src/userRoutes.ts
+++ apps/http-server/src/userRoutes.ts
@@ -3,6 +3,8 @@ import auth from './auth';
 import {PsClient} from '@repo/backend-common/db'
 import bcrypt, { hash } from 'bcrypt'
 import {UserSchema} from '@repo/common/zodSchema'
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET_KEY } from '@repo/backend-common/config';
 const userRouter = express.Router();
 
 userRouter.post("/signup",async( req,res)=>{
@@ -61,14 +63,48 @@ userRouter.post("/signup",async( req,res)=>{
 
 
 userRouter.post("/signin",async(req,res)=>{
-    const {username,password} = req.body;
-    // add the zod validation here
-    // add the db logic here
-    // add the jwt verification logic here
-    res.status(200).json({
-        message:"You are logged in ",
-    })
-    return;
+
+   try{
+            const dataValidation = UserSchema.safeParse(req.body);
+            if(!dataValidation.success){
+                res.status(403).json({
+                    message:" invalid credentials",
+                    error : dataValidation.error.message
+                })
+                return;
+            }
+
+            const {username,password,email} = req.body;
+
+            const user = await PsClient.user.findFirst({
+                where:{
+                    username,
+                    email
+                }
+            });
+
+            if(!user){
+                res.status(403).json({
+                    message: "You are not signed up, please sign up first"
+                })
+                return;
+            }
+
+            const token =  jwt.sign({id:user.id},JWT_SECRET_KEY);   // we will get the id in decoded.id
+            
+            res.status(200).json({
+                message:"You are logged in ",
+                token
+            })
+            return;
+   }
+   catch(error){
+        res.status(403).json({
+            //@ts-ignore
+            error: error.message
+        })
+        return;
+   }
 })
 
 userRouter.post("/create-room",auth,async(req,res)=>{
