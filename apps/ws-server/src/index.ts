e280395fc440655eaf238057bb177d84a2c410d9
Sumit <sumitsongh160@gmail.com>
2025-02-28 12:22:53 +0530

    Added the different endpoints in the ws layer

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -2,12 +2,18 @@
 import { WebSocketServer } from "ws";
 import jwt from 'jsonwebtoken'
 import {JWT_SECRET_KEY} from '@repo/backend-common/config'
+import {PsClient} from '@repo/backend-common/db';
 
 // Create a new WebSocket server listening on port 8080
 const wss = new WebSocketServer({ port: 8080 });
 
 console.log("server created");
-
+interface Users {
+    userId : int,
+    roomId : int[],
+    ws     : WebSocket
+}
+let users: Users[] = [];   // user will be the empty arror for now, later on we will add the details of the user as an object in it
 
 // Event listener for new client connections
 wss.on("connection", function connection(socket, request) {
@@ -32,12 +38,82 @@ wss.on("connection", function connection(socket, request) {
         return;
     }
 
+    users.push({
+         userId : decoded.userId,
+         roomId : [],   // intially the rooms of the user will be an empty array
+         ws: socket
+    })
+
 
     console.log("You are connected to the ws-server");
 
     // Listen for messages sent by the connected client
-    socket.on("message", function message(data) {
-        // Echo the received message back to the client
+    socket.on("message", async function message(data) {
+
+        if( data.type === "join_room" ) {
+            // if user want to join the particular room with room id he have mentioned in the message 
+            // we will push the room id in the room array for that particular user
+            // we should first check in the database for the existance of the room, and if it exist that only we need to let the
+            // user to join the room
+            const room = await PsClient.room.findFirst({
+                where: {
+                    id : data.roomId
+                }
+            })
+
+            if(!room) {
+                socket.send(JSON.stringify({
+                     message: "No such room exists, try joining another room"
+                }));
+
+                return;
+            }
+
+            const user = users.find((user)=> user.ws == socket);
+            if(!user){
+                socket.send(JSON.stringify({
+                    message: "Something went wrong"
+                }));
+                return;
+            }
+            // if the user exists in the users array
+            // push the roomid in the room array of the user
+            user.roomId.push(data.roomId);
+            ws.send("You have joined the room succesfully");
+            return;
+        }
+
+        else if(data.type === "chat"){
+            // this means the user want to send the message in the room with roomId present in the data
+            // we need to traverse to al the users which all are joined into the room with roomId present in the data
+            // then we need to send them message with the help of the sockets we havce stored in the object of particular user
+            // we also need to check if the current user also belong to the room to which he is sending the message
+            // for now we are allowing the user to send message to any of the room of his choice
+            users.forEach((user)=>{
+                if(user.roomId.includes(data.roomId)){
+                    // if the roomid array consist of the room to which the current user want to send the message then we will 
+                    // send the message to that user
+                    user.ws.send(data.message); // sended the message to the user with the help of his own socket which we have stored
+                }
+            })
+        }
+
+        else if(type === 'leave_room'){
+            // we will pop the the roomId provide from the users roomid array
+            users.forEach((user)=>{
+                if(user.ws === socket){
+                  user.roomId =  user.roomId.filter((room)=>room !== data.roomId);
+                }
+            })
+            return;
+        }
+
+        else{
+            // user might have send the wrong type 
+            ws.send("Entered wrong type")
+            return;
+        }
+        
         socket.send(data);
     });
 
