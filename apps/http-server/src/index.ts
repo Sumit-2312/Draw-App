f8a636bc2499a99dd8d8b339dde56caf44c3e014
Sumit <sumitsongh160@gmail.com>
2025-03-03 00:50:17 +0530

    Add integration logic in signup page

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { userRouter } from './userRoutes';
+import cors from 'cors'
 const app = express();
 app.use(express.json());
+app.use(cors()); // Allow all origins (for development)
 
 
 app.use("/user",userRouter);
--- apps/web/components/signupCard.tsx
+++ apps/web/components/signupCard.tsx
@@ -1,16 +1,43 @@
 "use client";
 import  Link  from "next/link";
+import { useRef } from "react";
+import axios from 'axios';
+import { useRouter } from "next/navigation";
+
 export default function Signup(){
+
+    const passref = useRef<HTMLInputElement>(null);
+    const emailref = useRef<HTMLInputElement>(null);  
+    const nameref = useRef<HTMLInputElement>(null);
+    const router = useRouter();
+
+    const handleclick = async()=>{
+        const name = nameref.current?.value;
+        const email = emailref.current?.value;
+        const password = passref.current?.value;
+
+        console.log(name,email,password);
+
+        const response = await axios.post("http://localhost:3001/user/signup",{
+            username:name,
+            email:email,
+            password:password
+        });
+        if(response.status === 200){
+            router.push('/signin');
+        }
+    }
+
     return(
         <div className="h-96 w-80 bg-zinc-300 rounded-xl">
             <div className="text-2xl text-black font-bold text-center p-5">
                 Sign up
             </div>
             <div className="flex flex-col gap-3 p-5">
-                <input type="text" placeholder="Enter name" className="p-3 rounded-lg border border-black"/>
-                <input type="email" placeholder="Enter email" className="p-3 rounded-lg border border-black"/>
-                <input type="password" placeholder="Enter password" className="p-3 rounded-lg border border-black"/>
-                <button className="bg-green-800 text-white p-3 rounded-lg">
+                <input ref={nameref} type="text" placeholder="Enter name" className="p-3 rounded-lg border border-black"/>
+                <input ref={emailref} type="email" placeholder="Enter email" className="p-3 rounded-lg border border-black"/>
+                <input ref={passref} type="password" placeholder="Enter password" className="p-3 rounded-lg border border-black"/>
+                <button onClick={handleclick} className="bg-green-800 text-white p-3 rounded-lg">
                     Sign up
                 </button>
             </div>
